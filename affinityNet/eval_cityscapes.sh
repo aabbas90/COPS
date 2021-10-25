0f39274481c098badd2d1d57b071b2fb586d441d
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2021-10-25 11:53:43 +0200

    coco_local

--- affinityNet/eval_cityscapes.sh
+++ affinityNet/eval_cityscapes.sh
@@ -12,10 +12,10 @@
 . ~/.bashrc_private
 eval "$(conda shell.bash hook)"
 # Activate a conda environment:
-conda activate conseg_public
+conda activate cops
 
 
-MODEL_DIR="output/cityscapes/fully_differentiable"
+MODEL_DIR="/home/ahabbas/projects/conseg_public/conseg/affinityNet/output/cityscapes/end_to_end_training_l_s_1_both_eps_more_layers_volta22"
 WEIGHTS="model_0002999.pth"
 OUT_FOLDER="model_0002999/"
 
@@ -28,7 +28,7 @@ python train_net.py \
     --num-gpus 1 --eval-only \
     MODEL.WEIGHTS ${MODEL_DIR}/${WEIGHTS} \
     OUTPUT_DIR ${MODEL_DIR}/${OUT_FOLDER} \
-    DATALOADER.EVAL_BATCH_SIZE 8 \
+    DATALOADER.EVAL_BATCH_SIZE 4 \
     DATALOADER.NUM_WORKERS 0 \
     MODEL.SAVE_RESULT_IMAGES False \
     DATASETS.TEST "('cityscapes_fine_panoptic_val',)"
